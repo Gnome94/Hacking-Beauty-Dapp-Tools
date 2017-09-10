1f7000aeaf4c1d7bd0a7d57640155ee4747ae7f0
Mark Muskardin <mark.muskardin@gmail.com>
2017-09-10 12:18:37 -0700

    added action/reducer for provider and added web3 instantiation logic
    test
    
    Tweaks to how web3 is called

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component }       from 'react';
 import { connect }                from 'react-redux';
+import { bindActionCreators }     from 'redux';
 import injectTapEventPlugin       from 'react-tap-event-plugin';
 import getMuiTheme                from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider           from 'material-ui/styles/MuiThemeProvider';
-import { BrowserRouter, Route }   from 'react-router-dom'
+import { HashRouter, Route }      from 'react-router-dom'
 import * as OfflinePluginRuntime  from 'offline-plugin/runtime';
-
+import Web3                       from 'web3';
 
 // global styles for entire app
 import './styles/app.scss';
@@ -14,6 +15,9 @@ import './styles/app.scss';
 import Header     from 'containers/Header';
 import LeftNavBar from 'containers/LeftNavBar';
 import Home       from 'containers/Home';
+
+/* actions */
+import * as providerActionCreators from 'core/actions/actions-provider';
 
 injectTapEventPlugin();
 
@@ -24,23 +28,45 @@ export class App extends Component {
     super(props);
   }
 
+  componentDidMount() {
+    const { actions } = this.props;
+    const currentProvider = window.web3.currentProvider;
+    let web3Provider;
+
+    if (typeof window.web3 !== 'undefined') {
+      web3Provider = new Web3(currentProvider);
+    } else {
+      alert('You must install MetaMask!')
+    }
+
+    actions.provider.specifyProvider(web3Provider);
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <div>
-          <Header />
-          <div className="container">
-            <BrowserRouter>
+        <HashRouter>
+          <div>
+            <Header />
+            <div className="container">
               <div>
                 <Route exact path="/" component={Home}/>
               </div>
-            </BrowserRouter>
+            </div>
+            <LeftNavBar />
           </div>
-          <LeftNavBar />
-        </div>
+        </HashRouter>
       </MuiThemeProvider>
     );
   }
 }
 
-export default connect(null)(App);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      provider: bindActionCreators(providerActionCreators, dispatch)
+    }
+  };
+}
+
+export default connect(null, mapDispatchToProps)(App);
